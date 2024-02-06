59115b9eb809ee4cad7efbc10525d6804c5b0387
Hashmat Ibrahimi <hashmatibrahimi0711@gmail.com>
2024-02-05 20:34:59 -0500

    Minor change for redeployment to vercel

--- src/components/InfiniteTweetsList.tsx
+++ src/components/InfiniteTweetsList.tsx
@@ -117,7 +117,7 @@ function TweetCard({
       <div className="flex flex-grow flex-col">
         <div className="flex gap-1">
           <Link
-            href={`/profiles/${user.id}`}
+            href={`/test/${user.id}`}
             className="font-bold outline-none hover:underline focus-visible:underline"
           >
             {user.name}
